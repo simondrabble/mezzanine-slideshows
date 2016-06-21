35e96644208e64bfad0a2b0ae5543685d4cb9a83
Simon Drabble <simon.drabble@gmail.com>
2016-06-21 08:52:20 -0600

    Use new fields in template. Version bump.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='mezzanine-slideshows',
-    version='0.3.2',
+    version='0.3.6',
     packages=['mezzanine_slideshows'],
     include_package_data=True,
     license='BSD License',
@@ -32,4 +32,3 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
 )
-
